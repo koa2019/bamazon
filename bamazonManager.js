29e6a234ef404daa1f093d1a0354fdf847f9fd43
koa2019 <no-reply>
2019-09-07 09:41:46 -0700

    created new js for bamamzon manager & consoled if connection to mySql was successful or not

---
+++ bamazonManager.js
@@ -0,0 +1,23 @@
+var inqurier = require('inquirer');
+
+var mysql = require('mysql2');
+
+var connection = mysql.createConnection({
+    host: "localhost",
+
+    // Your port; if not 3306
+    port: 3306,
+
+    // Your username
+    user: "root",
+
+    // Your password
+    password: "",
+    database: "bamazon"
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log('Success. Connection id: ' + connection.threadId);
+
+});
