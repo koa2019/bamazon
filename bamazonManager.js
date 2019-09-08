cbf6a9ffb67879e82ad8e801d90bbec1f539caa2
koa2019 <no-reply>
2019-09-08 12:16:51 -0700

    added switch statement to take user's menu selection & call a function dependent on it's case

--- bamazonManager.js
+++ bamazonManager.js
@@ -5,39 +5,82 @@ var mysql = require('mysql2');
 var connection = mysql.createConnection({
     host: "localhost",
 
-    // Your port; if not 3306
     port: 3306,
 
-    // Your username
     user: "root",
 
-    // Your password
     password: "",
     database: "bamazon"
 });
 
+//connect to server. if successful console threadId & call function
 connection.connect(function(err) {
     if (err) throw err;
     console.log('Success. Connection id: ' + connection.threadId);
-    showProducts();
+    // showProducts();
+    promptManager();
 });
 
-inqurier.prompt([{
+function promptManager() {
+    //console  menu selections
+    inqurier.prompt([{
 
-    type: 'list',
-    name: 'menu',
-    message: 'Manager Menu. Select one: ',
-    choices: ['View Products for Sale', 'View Low Inventory', 'Add to Inventory', 'Add New Product']
+        type: 'list',
+        name: 'menu',
+        message: 'Manager Menu. Select one: ',
+        choices: ['View Products for Sale', 'View Low Inventory', 'Add to Inventory', 'Add New Product']
 
-}]).then(function(resp) {
+    }]).then(function(resp) {
 
-    console.log(resp);
+        console.log(resp.menu);
+        doThis(resp.menu);
 
-}).catch(function(err) {
+    }).catch(function(err) {
+        if (err) throw err;
+        connection.end();
+    });
+}
 
-    if (err) throw err;
-    connection.end();
-});
+function doThis(action) {
+
+    switch (action) {
+        case 'View Products for Sale':
+            productsForSale();
+            break;
+        case 'View Low Inventory':
+            lowInventory();
+            break;
+        case 'Add to Inventory':
+            addToInventory();
+            break;
+        case 'Add New Product':
+            addNewProd();
+            break;
+    };
+}
+
+function productsForSale() {
+
+    console.log('4 sale');
+}
+
+function lowInventory() {
+
+    console.log('low stock');
+
+}
+
+function addToInventory() {
+
+    console.log('add to ');
+
+}
+
+function addNewProd() {
+
+    console.log('add new');
+
+}
 
 function showProducts() {
     connection.query("SELECT * FROM products", function(err, res) {
